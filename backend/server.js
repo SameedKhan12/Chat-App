33c81c451ef3111b10c216b5a9148cea1e2c5c7b
SameedKhan12 <eedsam0@gmil.com>
2024-03-25 14:05:00 +0500

    d 3

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,8 @@ app.use("/api/message", messageRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
-app.use(express.static(path.join(__dirname,"./frontend/dist")));
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
 app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 })
